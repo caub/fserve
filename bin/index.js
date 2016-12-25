4d226eddad9c2680edd5cb2d63e05432063aac50
caub <cyril.auburtin@gmail.com>
2016-12-25 18:36:48 +0100

    updated with serveStatic

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
+const http = require('http');
+const serveStatic = require('serve-static');
 
-var express = require('express');
 
-express()
-.use(express.static('.'))
-.listen(parseInt(process.argv[2])||3000);
+const serve = serveStatic('.', {extensions: ['html']});
+
+const errhandler = (req, res) => err => {
+	res.setHeader('X-Content-Type-Options', 'nosniff');
+	res.statusCode = 404;
+	res.end();
+}
+
+
+const server = http.createServer((req, res) => {
+	serve(req, res, errhandler(req, res))
+});
+
+
+server.listen(parseInt(process.argv[2])||3000);
+
